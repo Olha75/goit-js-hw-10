2a5f6574ff1fbde91bf219c8e888b8d12a0aca9e
Olha75 <137830839+Olha75@users.noreply.github.com>
2023-11-22 21:43:36 +0200

    ax-fo

---
+++ src/cat-api.js
@@ -0,0 +1,4 @@
+// fetchBreeds()
+// https://api.thecatapi.com/v1/images/search
+// breed_ids
+// https://api.thecatapi.com/v1/images/search?breed_ids=ідентифікатор_породи
--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import Notiflix from 'notiflix';
-import SlimSelect from 'slim-select'
+import SlimSelect from 'slim-select';
 
 new SlimSelect({
-  select: '#selectElement'
-})
+  select: '#selectElement',
+});
+
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "live_9hI5TC9HPYJRhH8Gtg4eTcngwpwFFfjAMbdbyKywZKhWDoRzX1TTnnEkwSlVw6aA";
+
+// https://api.thecatapi.com/v1/breeds
+// select.breed-select
+
+
+
+// live_9hI5TC9HPYJRhH8Gtg4eTcngwpwFFfjAMbdbyKywZKhWDoRzX1TTnnEkwSlVw6aA
+
+// Use it as the 'x-api-key' header when making any request to the API, or by adding as a query string parameter e.g. 'api_key=live_9hI5TC9HPYJRhH8Gtg4eTcngwpwFFfjAMbdbyKywZKhWDoRzX1TTnnEkwSlVw6aA'
