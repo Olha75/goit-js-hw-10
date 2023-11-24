5d8c97baeb537d5b136a84391a8a565c3e259802
Olha75 <137830839+Olha75@users.noreply.github.com>
2023-11-24 18:41:47 +0200

    no-text

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,36 +1,36 @@
-import axios from "axios";
 const API_URL = 'https://api.thecatapi.com/v1';
 
-
 function fetchBreeds() {
-    return fetch(`${API_URL}/breeds`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Network response was not ok: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(data => data)
-      .catch(error => {
-        console.error('Error fetching breeds:', error);
-        throw error;
-      });
-  }
-  
-  function fetchCatByBreed(breedId) {
-    return fetch(`${API_URL}/images/search?breed_ids=${breedId}`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Network response was not ok: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(data => data)
-      .catch(error => {
-        console.error('Error fetching cat by breed:', error);
-        throw error;
-      });
-  }
+  return fetch(`${API_URL}/breeds`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => data)
+    .catch(error => {
+      console.error('Error fetching breeds:', error);
+      throw error;
+    });
+}
+
+function fetchCatByBreed(breedId) {
+  return fetch(`${API_URL}/images/search?breed_ids=${breedId}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => data)
+    .catch(error => {
+      console.error('Error fetching cat by breed:', error);
+      throw error;
+    });
+}
+
+export { fetchBreeds, fetchCatByBreed };
 
 
 // https://api.thecatapi.com/v1/images/search
--- src/index.js
+++ src/index.js
@@ -11,22 +11,57 @@ import axios from "axios";
 
 axios.defaults.headers.common["x-api-key"] = "live_9hI5TC9HPYJRhH8Gtg4eTcngwpwFFfjAMbdbyKywZKhWDoRzX1TTnnEkwSlVw6aA";
 
+document.addEventListener('DOMContentLoaded', () => {
+  const breedSelect = document.querySelector('.breed-select');
+  const loader = document.querySelector('.loader');
+  const error = document.querySelector('.error');
+  const catInfo = document.querySelector('.cat-info');
+
+  fetchBreeds()
+    .then(breeds => {
+      breeds.forEach(breed => {
+        const option = document.createElement('option');
+        option.value = breed.id;
+        option.textContent = breed.name;
+        breedSelect.appendChild(option);
+      });
+      loader.style.display = 'none';
+    })
+    .catch(() => {
+      loader.style.display = 'none';
+      error.style.display = 'block';
+    });
+
+  breedSelect.addEventListener('change', () => {
+    const selectedBreedId = breedSelect.value;
+    catInfo.innerHTML = '';
+
+    fetchCatByBreed(selectedBreedId)
+      .then(cats => {
+        const cat = cats[0];
+        const img = document.createElement('img');
+        img.src = cat.url;
+        img.alt = 'Cat';
+        catInfo.appendChild(img);
+
+        const infoParagraph = document.createElement('p');
+        infoParagraph.textContent = `Порода: ${cat.breeds[0].name}, Опис: ${cat.breeds[0].description}, Темперамент: ${cat.breeds[0].temperament}`;
+        catInfo.appendChild(infoParagraph);
+      })
+      .catch(error => {
+        console.error('Помилка при отриманні інформації про кота за породою:', error);
+      });
+  });
+});
+
+
+
+
 // GET https://api.thecatapi.com/v1/breeds
 // select.breed-select
-const refs = {
-  form: document.querySelector(".js-search-form "),
-  list: document.querySelector(".js-list"),
-};
-
-refs.form.addEventListener("submit", handleSearch);
-
-function handleSearch(event) {
-  event.preventDefault();
-  const {breeds, opus}=event.currentTarget.elements;
 
 
 
-}
 
 // live_9hI5TC9HPYJRhH8Gtg4eTcngwpwFFfjAMbdbyKywZKhWDoRzX1TTnnEkwSlVw6aA
 
