bbe61f6e214740e591f4d38e7401ae4156a93907
Olha75 <137830839+Olha75@users.noreply.github.com>
2023-11-24 23:31:46 +0200

    no-style

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
+
 import Notiflix from 'notiflix';
 import SlimSelect from 'slim-select';
-import { fetchBreeds, fetchCatByBreed, fetchBreedDetails  } from './cat-api.js';
-
+import { fetchBreeds, fetchCatByBreed, fetchBreedDetails } from './cat-api.js';
 
 new SlimSelect({
   select: '#breedSelect',
@@ -46,9 +46,17 @@ document.addEventListener('DOMContentLoaded', () => {
         img.alt = 'Cat';
         catInfo.appendChild(img);
 
-        const infoParagraph = document.createElement('p');
-        infoParagraph.textContent = `Порода: ${breedDetails.name}, Опис: ${breedDetails.description}, Темперамент: ${breedDetails.temperament}`;
-        catInfo.appendChild(infoParagraph);
+        const breedTitle = document.createElement('h1');
+        breedTitle.textContent = breedDetails.name;
+        catInfo.appendChild(breedTitle);
+
+        const descriptionParagraph = document.createElement('p');
+        descriptionParagraph.textContent = `Опис: ${breedDetails.description}`;
+        catInfo.appendChild(descriptionParagraph);
+
+        const temperamentParagraph = document.createElement('p');
+        temperamentParagraph.textContent = `Темперамент: ${breedDetails.temperament}`;
+        catInfo.appendChild(temperamentParagraph);
       })
       .catch(error => {
         console.error('Помилка при отриманні інформації про кота за породою:', error);
@@ -58,6 +66,63 @@ document.addEventListener('DOMContentLoaded', () => {
       });
   });
 });
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const breedSelect = document.querySelector('.breed-select');
+//   const loader = document.querySelector('.loader');
+//   const error = document.querySelector('.error');
+//   const catInfo = document.querySelector('.cat-info');
+
+//   fetchBreeds()
+//     .then(breeds => {
+//       breeds.forEach(breed => {
+//         const option = document.createElement('option');
+//         option.value = breed.id;
+//         option.textContent = breed.name;
+//         breedSelect.appendChild(option);
+//       });
+//       loader.style.display = 'none';
+//     })
+//     .catch(() => {
+//       loader.style.display = 'none';
+//       error.style.display = 'block';
+//     });
+
+//   breedSelect.addEventListener('change', () => {
+//     const selectedBreedId = breedSelect.value;
+//     catInfo.innerHTML = '';
+
+//     loader.style.display = 'block';
+
+//     Promise.all([fetchCatByBreed(selectedBreedId), fetchBreedDetails(selectedBreedId)])
+//       .then(([cats, breedDetails]) => {
+//         const cat = cats[0];
+//         const img = document.createElement('img');
+//         img.src = cat.url;
+//         img.alt = 'Cat';
+//         catInfo.appendChild(img);
+
+//         const infoParagraph = document.createElement('p');
+//         infoParagraph.textContent = `Порода: ${breedDetails.name}, Опис: ${breedDetails.description}, Темперамент: ${breedDetails.temperament}`;
+//         catInfo.appendChild(infoParagraph);
+//       })
+//       .catch(error => {
+//         console.error('Помилка при отриманні інформації про кота за породою:', error);
+//       })
+//       .finally(() => {
+//         loader.style.display = 'none';
+//       });
+//   });
+// });
 
 
 
